34ea4dd428879b27540abe74673cbd75691645b2
wanjiku <annritawanjiku77@gmail.com>
2023-04-12 18:13:59 +0300

    developed signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
+import {Link} from 'react-router-dom';
 
 export default function SignIn() {
     const [showPassword, setShowPassword] = useState(false)
@@ -36,6 +37,7 @@ export default function SignIn() {
                     onChange={onChange}
                     placeholder="Email Address"
                     className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out '/>
+                   
                     <div className='relative mb-6'>
                         <input  
                         type={showPassword ? "text": "password"} 
@@ -48,6 +50,23 @@ export default function SignIn() {
                         {showPassword ? <AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=> setShowPassword((prevState) =>!prevState)}/> : <AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=> setShowPassword((prevState) =>!prevState)}/>}
 
                     </div>
+
+                    <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+                        <p className='mb-6'>Don't have an account? 
+                            <Link to="/sign-up" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out ml-1'> Register</Link>
+                        </p>
+                        <p>
+                            <Link to="/forgot-password" className='text-red-600 hover:text-red-800 transition duration-200 ease-in-out'> Forgot password?</Link>
+                        </p>
+                    </div>
+
+                    <button className='w-full bg-blue-600 text-white text-sm font-medium uppercase px-7 py-3 rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'
+                    type='submit'>Sign in</button>
+
+                    <div className='flex items-center my-4 before:border-t  before:flex-1  before:border-gray-300 after:border-t  after:flex-1  after:border-gray-300'>
+                        <p className='text-center font-semibold mx-4'>OR</p>
+                    </div>
+
                 </form>
             </div>
         </div>
