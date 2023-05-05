41ad465a47ebf26bf09927539a721d062f6b94f2
wanjiku <annritawanjiku77@gmail.com>
2023-05-05 07:05:46 +0300

    complete listing item component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -119,8 +119,8 @@ export default function Profile() {
       <div className='max-w-6xl px-3 mt-6 mx-auto'>
         {!loading && listings.length > 0 &&(
           <>
-            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
-            <ul>
+            <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-gird-cols-5 mt-6 mb-6'>
               {listings.map((listing)=>(
                 <ListingItem 
                   key={listing.id} 
