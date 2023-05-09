4c5b3c332a815fa172573e043889e50ccb123a9d
wanjiku <annritawanjiku77@gmail.com>
2023-05-09 10:28:34 +0300

    added share and copylink funtionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,12 +8,14 @@ import { db } from '../firebase'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from 'swiper'
 import 'swiper/css/bundle'
+import {FaShare} from 'react-icons/fa'
 
 export default function Listing() {
 
     const params = useParams()
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
     // initialize swiper
     SwiperCore.use([Autoplay, Navigation, Pagination])
     
@@ -46,6 +48,27 @@ export default function Listing() {
                     </div>
                 </SwiperSlide>
             ))}
-        </Swiper>{listing.name}</main>
+        </Swiper>
+        {/* sharing icon */}
+        <div 
+            className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center' 
+            onClick={()=>{
+                navigator.clipboard.writeText(window.location.href)
+                setShareLinkCopied(true)
+                setTimeout(()=>{
+                    setShareLinkCopied(false)
+                }, 2000)
+            }}>
+                    <FaShare className='text-lg text-slate-500'/>
+        </div>
+        {shareLinkCopied && (
+            <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied</p>
+        )}
+        
+        
+        {listing.name}
+        
+        </main>
+        
   )
 }
