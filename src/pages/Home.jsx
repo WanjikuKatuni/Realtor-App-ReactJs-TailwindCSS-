b67712d204cd085a6d2b86686452a0df74bd0a99
wanjiku <annritawanjiku77@gmail.com>
2023-05-12 00:19:38 +0300

    responsivity edits in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -123,7 +123,7 @@ export default function Home() {
             <Link to="/offers">
               <p className='px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out'>Show more offers</p>
             </Link>
-            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 cl:grid-cols-4'>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
               {offerListings.map((listing)=>(
                 <ListingItem 
                   key={listing.id} 
@@ -138,7 +138,7 @@ export default function Home() {
             <Link to="/category/rent">
               <p className='px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out'>Show more places for rent</p>
             </Link>
-            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 cl:grid-cols-4'>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
               {rentListings.map((listing)=>(
                 <ListingItem 
                   key={listing.id} 
@@ -153,7 +153,7 @@ export default function Home() {
             <Link to="/category/sale">
               <p className='px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out'>Show more places for sale</p>
             </Link>
-            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 cl:grid-cols-4'>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
               {saleListings.map((listing)=>(
                 <ListingItem 
                   key={listing.id} 
